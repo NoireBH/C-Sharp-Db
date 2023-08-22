f88cb25680f9b5c0c85cf79b46c8540a513d8e62
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-08-22 19:02:32 +0300

    added exercises

--- 05. Subqueries and Joins.sql
+++ 05. Subqueries and Joins.sql
@@ -105,3 +105,171 @@ WHERE ep.[ProjectID]
 	AND p.StartDate > '2002-08-13'
 	AND p.EndDate IS NULL
 ORDER BY e.[EmployeeID]
+
+--8
+
+SELECT
+	e.[EmployeeID],
+	e.[FirstName],	
+	CASE 
+		WHEN p.[StartDate] >= '2005-01-01' THEN NULL
+		ELSE p.[Name]
+	END 
+	AS [ProjectName]
+FROM [Employees] 
+	AS [e]
+JOIN [EmployeesProjects] 
+	AS [ep]
+	ON e.[EmployeeID] = ep.[EmployeeID]
+JOIN [Projects] 
+	AS [p]
+	ON ep.[ProjectID] = p.[ProjectID]
+WHERE e.[EmployeeID] = 24
+
+--9
+
+SELECT
+	e.[EmployeeID],
+	e.[FirstName],
+	e.[ManagerID],
+	em.[FirstName] AS [ManagerName]
+FROM [Employees]
+	AS e
+JOIN [Employees]
+	AS em
+	ON em.EmployeeID = e.ManagerID
+WHERE e.ManagerID IN (3,7)
+ORDER BY e.EmployeeID ASC
+
+--10
+
+USE [SoftUni]
+
+SELECT
+	TOP (50)
+	e.EmployeeID,
+	CONCAT_WS(' ',e.FirstName,e.LastName) AS [EmployeeName],
+	CONCAT_WS(' ',em.FirstName,em.LastName) AS [ManagerName],
+	d.[Name] AS [DepartmentName]
+FROM [Employees]
+	AS e
+JOIN [Employees]
+	AS em
+	ON em.EmployeeID = e.ManagerID
+JOIN [Departments]
+	AS d
+	ON e.DepartmentID = d.DepartmentID
+ORDER BY e.[EmployeeID]
+
+--11
+
+SELECT
+	MIN(a.AverageSalary) AS MinAverageSalary
+FROM
+(
+	SELECT 
+		e.[DepartmentID],
+		AVG(e.[Salary]) AS [AverageSalary]
+	FROM [Employees] AS [e]
+	GROUP BY e.[DepartmentID]
+) AS [a]
+
+--12
+
+USE [Geography]
+
+SELECT
+	mc.CountryCode,
+	m.MountainRange,
+	p.PeakName,
+	p.Elevation
+FROM [MountainsCountries]
+	AS [mc]
+JOIN [Mountains]
+	AS [m]
+	ON [m].Id = [mc].MountainId
+JOIN [Peaks]
+	AS [p]
+	ON [p].MountainId = [mc].MountainId
+WHERE [mc].CountryCode = 'BG' AND [p].Elevation > 2835
+ORDER BY [p].Elevation DESC
+
+--13
+
+SELECT
+	mc.CountryCode,
+	COUNT(m.[MountainRange]) AS [MountainRanges]
+FROM [MountainsCountries]
+	AS mc
+JOIN [Mountains]
+	AS m
+	ON mc.MountainId = m.Id
+WHERE mc.CountryCode IN ('BG','RU','US')
+GROUP BY mc.CountryCode
+
+--14
+
+SELECT
+	TOP (5)
+	c.CountryName,
+	r.RiverName
+FROM [Countries]
+	AS c
+LEFT JOIN [CountriesRivers]
+	AS cr
+	ON c.CountryCode = cr.CountryCode
+LEFT JOIN [Rivers]
+	AS r
+	ON r.Id = cr.RiverId
+WHERE c.[ContinentCode] = 'AF'
+ORDER BY c.CountryName
+
+--15
+
+SELECT
+	*
+FROM [Countries]
+
+
+--16
+
+
+
+SELECT
+	COUNT(c.[CountryCode]) AS [Count]
+FROM [Countries]
+	AS c
+LEFT JOIN [MountainsCountries]
+	AS mc
+	ON c.CountryCode = mc.CountryCode
+LEFT JOIN [Mountains]
+	as m
+	ON mc.MountainId = m.Id
+WHERE mc.MountainId IS NULL
+
+--17
+
+SELECT
+	TOP (5)
+	c.[CountryName],
+	MAX(p.[Elevation]) AS [HighestPeakElevation],
+	MAX(r.[Length]) AS [LongestRiverLength]
+FROM [Countries]
+	AS [c]
+JOIN [MountainsCountries]
+	AS mc
+	ON mc.CountryCode = c.CountryCode
+JOIN [Peaks]
+	AS [p]
+	ON p.MountainId = mc.MountainId
+JOIN [CountriesRivers]
+	AS [cr]
+	ON cr.CountryCode = c.CountryCode
+JOIN [Rivers] 
+	AS [r]
+	ON r.Id = cr.RiverId
+GROUP BY [CountryName]
+ORDER BY 
+	[HighestPeakElevation] DESC,
+	[LongestRiverLength] DESC,
+	[CountryName] ASC
