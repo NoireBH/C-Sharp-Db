6e833d46ad0ee60da33e17108f2481691933a56c
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-08-24 11:14:50 +0300

    added exercises

--- 05. Subqueries and Joins.sql
+++ 05. Subqueries and Joins.sql
@@ -227,13 +227,37 @@ ORDER BY c.CountryName
 --15
 
 SELECT
-	*
-FROM [Countries]
+	ContinentCode,
+	CurrencyCode,
+	CurrencyUsage
 
+FROM
+
+(
+SELECT 
+	*,
+	DENSE_RANK() OVER (PARTITION BY ContinentCode ORDER BY CurrencyUsage DESC)
+	AS CurrencyRank
+FROM
+( 
+	SELECT
+		c.ContinentCode,
+		c.CurrencyCode,
+		COUNT(*) 
+			AS [CurrencyUsage]
+
+	FROM [Countries]
+		AS c
+	GROUP BY c.[ContinentCode],c.[CurrencyCode]
+	HAVING COUNT(c.[CurrencyCode]) > 1
+) 
+	AS CurrencyUsageSubquery
+) 
+	AS CurrencyRankSubquery
+
+WHERE [CurrencyRank] = 1
 
 --16
-
-
 
 SELECT
 	COUNT(c.[CountryCode]) AS [Count]
@@ -273,3 +297,44 @@ ORDER BY
 	[HighestPeakElevation] DESC,
 	[LongestRiverLength] DESC,
 	[CountryName] ASC
+
+--18
+
+SELECT 
+	TOP (5)
+	[Country],
+	CASE
+		WHEN [PeakName] IS NULL THEN '(no highest peak)'
+		ELSE [PeakName]
+	END AS [Highest Peak Name],
+	CASE
+		WHEN [Elevation] IS NULL THEN 0
+		ELSE [Elevation]
+	END AS [Highest Peak Elevation],
+	CASE
+		WHEN [MountainRange] IS NULL THEN '(no mountain)'
+		ELSE [MountainRange]
+	END AS [Mountain]
+FROM
+(
+	SELECT
+		c.[CountryName] AS [Country],
+		m.[MountainRange],
+		p.[PeakName],
+		p.[Elevation],
+		DENSE_RANK() OVER
+		(
+			PARTITION BY c.[CountryName] 
+			ORDER BY p.[Elevation] DESC
+		) AS [PeakRank]
+	FROM [Countries]
+		AS c
+		LEFT JOIN [MountainsCountries] AS [mc]
+		ON mc.[CountryCode] = c.[CountryCode]
+	LEFT JOIN [Mountains] AS [m]
+		ON mc.[MountainId] = m.[Id]
+	LEFT JOIN [Peaks] AS [p]
+		ON p.[MountainId] = m.[Id]
+)	AS [PeakRankingTable]
+WHERE [PeakRank] = 1
+ORDER BY [Country], [Highest Peak Name] 
