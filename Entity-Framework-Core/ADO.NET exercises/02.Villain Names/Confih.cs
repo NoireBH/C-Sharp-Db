15aa4d3d1487c36b7c8e932b3b1d95e5e4a54e83
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-09-14 16:35:49 +0300

    added project solutions

---
+++ Entity-Framework-Core/ADO.NET exercises/02.Villain Names/Confih.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace _02.Villain_Names
+{
+    internal static class Confih
+    {
+        public const string connectionString = @"Server=DESKTOP-84CHDHM\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
