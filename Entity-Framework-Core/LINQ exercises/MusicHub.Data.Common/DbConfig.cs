87e926406ae574e52eda3a9deb4c393c13b2afd7
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-10-03 16:48:02 +0300

    replace musicDb with the right skeleton

--- Entity-Framework-Core/LINQ exercises/MusicHub.Data.Common/DbConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicHub.Data.Common
-{
-    public class DbConfig
-    {
-        public const string ConnectionString =
-            @"Server=DESKTOP-84CHDHM\SQLEXPRESS;Database=MusicHub;Integrated Security=True;TrustServerCertificate=True;Encrypt=False;";
-    }
-}
