8403ac61be0253c44c77b4efb7a871d1da22e9c5
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-11-01 17:21:42 +0200

    added export dto's and export logic

--- Entity-Framework-Core/Exam prep/C# DB Advanced Exam - 08 August 2020/01. Model Definition_Skeleton/VaporStore/DataProcessor/Serializer.cs
+++ Entity-Framework-Core/Exam prep/C# DB Advanced Exam - 08 August 2020/01. Model Definition_Skeleton/VaporStore/DataProcessor/Serializer.cs
@@ -1,8 +1,13 @@
 ﻿namespace VaporStore.DataProcessor
-{ 
+{
     using Data;
     using Newtonsoft.Json;
+    using SoftJail.Utilities;
+    using System.Globalization;
+    using System.Text;
     using VaporStore.Data.Models;
+    using VaporStore.Data.Models.Enums;
+    using VaporStore.DataProcessor.ExportDto;
 
     public static class Serializer
     {
@@ -44,7 +49,47 @@
 
         public static string ExportUserPurchasesByType(VaporStoreDbContext context, string purchaseType)
         {
-            throw new NotImplementedException();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            PurchaseType purchaseTypeEnum = Enum.Parse<PurchaseType>(purchaseType);
+
+            var exportPurchases = context.Users
+                .ToArray()
+                .Where(u => u.Cards.Any(c => c.Purchases.Any()))
+                .Select(u => new ExportUserDto()
+                {
+                    Username = u.Username,
+                    Purchases = context
+                        .Purchases
+                        .ToArray()
+                        .Where(p => p.Card.User.Username == u.Username && p.Type == purchaseTypeEnum)
+                        .OrderBy(p => p.Date)
+                        .Select(p => new ExportUserPurchaseDto()
+                        {
+                            CardNumber = p.Card.Number,
+                            CardCvc = p.Card.Cvc,
+                            Date = p.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                            Game = new ExportUserPurchaseGameDto()
+                            {
+                                Name = p.Game.Name,
+                                Genre = p.Game.Genre.Name,
+                                Price = p.Game.Price
+                            }
+
+                        })
+                        .ToArray(),
+                    TotalSpent = context
+                        .Purchases
+                        .ToArray()
+                        .Where(p => p.Card.User.Username == u.Username && p.Type == purchaseTypeEnum)
+                        .Sum(p => p.Game.Price)
+                })
+                .Where(u => u.Purchases.Length > 0)
+                .OrderByDescending(u => u.TotalSpent)
+                .ThenBy(u => u.Username)
+                .ToArray();
+
+            return xmlHelper.Serialize<ExportUserDto[]>(exportPurchases, "Users").TrimEnd();
         }
     }
 }
